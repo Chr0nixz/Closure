366e6a197290d905009e5f54f4d315fab6940c8a
陈泽煊 <czxxxcyz@163.com>
2023-01-18 21:30:57 +0800

    v1.3.4 服务器要维护了

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
 import sys
-import ctypes
+import time
 
+from ctypes import windll
+from os import getcwd, path
 from PyQt5.QtWidgets import QApplication
 from qt_material import apply_stylesheet
 
@@ -11,8 +12,8 @@ from resources.lib import event, config, gamedata, cache, constant, update
 from resources.style import style
 
 if __name__ == '__main__':
-
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("Chr0nix.Closure.Helper.v1.3")
+    start = time.process_time()
+    windll.shell32.SetCurrentProcessExplicitAppUserModelID("Chr0nix.Closure.Helper.v1.3")
 
     app = QApplication(sys.argv)
     apply_stylesheet(app, theme='dark_amber.xml', extra=style.extra)
@@ -20,13 +21,16 @@ if __name__ == '__main__':
     app.setStyleSheet(stylesheet + style.stylesheet)
 
     controller = MainController()
-    controller.setCache(cache.Cache(os.getcwd()))
-    configs = config.Config(os.path.join(os.getcwd(), 'Config.json'))
-    windows = WindowController(controller, os.path.dirname(__file__))
+    controller.setCache(cache.Cache(getcwd()))
+    configs = config.Config(path.join(getcwd(), 'Config.json'))
+    windows = WindowController(controller, path.dirname(__file__))
     event.init(controller, windows, configs)
-    updates = update.Update(os.getcwd(), windows)
-    gamedata.init(os.path.join(os.path.dirname(__file__), 'resources', 'json'))
-    constant.init(os.getcwd())
+    updates = update.Update(getcwd(), windows)
+    gamedata.init(path.join(path.dirname(__file__), 'resources', 'json'))
+    constant.init(getcwd())
+
+    end = time.process_time()
+    print(end-start)
 
     updates.start()
     sys.exit(app.exec_())
